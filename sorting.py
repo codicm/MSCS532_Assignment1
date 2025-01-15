8c6bd53c2294756b3b1dc1a1f46fc45aec602e03
codicm <codictech@gmail.com>
2025-01-15 13:48:22 -0600

    second commit

---
+++ sorting.py
@@ -0,0 +1,19 @@
+def insertion_sort_desc(arr):
+
+    for i in range(1, len(arr)):
+        key = arr[i]
+        z = i - 1
+        
+        while z >= 0 and arr[z] < key:
+            arr[z + 1] = arr[z]
+            z -= 1
+        arr[z + 1] = key
+    return arr
+
+
+
+if __name__ == "__main__":
+    data = [5, 2, 9, 1, 5, 6]
+    print("Original array:", data)
+    sorted_data = insertion_sort_desc(data)
+    print("Sorted array (Descending):", sorted_data)
